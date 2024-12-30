e2f852953bbb05c7e280df7cc49fc72a0f671559
Minhaj <minhajdudekula007@gmail.com>
2024-12-31 01:56:27 +0530

    created header and body

--- App.js
+++ App.js
@@ -1,3 +1,71 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import img from "./pushpa2.jpg"
 
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="app-name" >MarWell</div>
+            <div className="nav-items">
+                <ul>
+                <li>Home</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Sports</li>
+                <li>Login</li>
+                <li>Sighin</li>
+                </ul>
+                
+            </div>
+        </div>
+    );
+};
+
+const MovieCard = () => {
+    return (
+        <div className="card">
+            <img className="poster" src={img} alt="movie poster"/>
+            <h2>Pushpa 2</h2>
+            <h3>8.5/10</h3>
+            <h3>3:22 hrs</h3>
+        </div>
+    );
+};
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search-section">
+                <div className="search">search and button</div>
+                <div className="years">Year Filter</div>
+                <div className="jonars">Jonars list</div>
+                <div className="language">Language</div>
+
+            </div>
+            <div className="card-container">
+                <MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/><MovieCard/>
+            </div>
+        </div>
+    );
+};
+
+const Footer = () => {
+    return (
+        <div></div>
+    );
+};
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
+            <Footer/>
+        </div>
+    );
+};
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout/>)
