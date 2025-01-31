5ea955dfee934f54d88e9df2acc51a9fc793df63
Minhaj <minhajdudekula007@gmail.com>
2025-01-31 20:36:03 +0530

    dynamic search box

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,6 +9,10 @@ const Body = () => {
 
     const [listOfMovies, setListOfMovies] = useState(moviesList);
 
+    const [filteredMovies, setFilteredMovies] = useState(moviesList);
+
+    const [searchText, setSearchText] = useState("");
+
     // useEffect(() => {
     //     fetchData();
     // }, []);
@@ -17,16 +21,9 @@ const Body = () => {
     // const fetchData = async () => 
     // {
     //     const data = await fetch("https://www.hotstar.com/api/internal/bff/v2/pages/search?slug=in&slug=explore&search_query=matvel&referrer_props=%7B%22search_session_id%22%3A%22b3e6570f-0721-4269-8dfb-d79cc76120e5%22%2C%22search_id%22%3A%22b3e6570f-0721-4269-8dfb-d79cc76120e5-1%22%7D", {method: "POST", body: JSON.stringify({ username: "example"})});
-
-
     //     const json = await data.json();
     //     // const {items} = json?.success?.page?.spaces?.results?.widget_wrappers[0]?.widget?.data?.
-
-        
-        
-    //     setFilterAsYear(json?.success?.page?.spaces?.results?.widget_wrappers[0]?.widget?.data?.items);
-        
-         
+    //     setFilterAsYear(json?.success?.page?.spaces?.results?.widget_wrappers[0]?.widget?.data?.items);    
     // };
 
     if (listOfMovies.length === 0 ) {
@@ -37,7 +34,20 @@ const Body = () => {
     return (
         <div className="body">
             <div className="search-section">
-                <div className="search">search and button</div>
+
+                <div className="search">
+                    <input type="text" className="search-box" value={searchText}
+                    onChange={(e) => {
+                        setSearchText(e.target.value);
+                    }}></input>
+
+                    <button onClick={() => {
+                        const filteredMovies = listOfMovies.filter((movie) => movie?.search_horizontal_content_card?.data?.title?.toLowerCase()?.includes(searchText.toLowerCase()));
+
+                        setFilteredMovies(filteredMovies);
+                    }} >search</button>
+                </div>
+
                 <div className="years">
                     <button className="year_filter" onClick={
                         () => {const filteredList = listOfMovies.filter(
@@ -51,7 +61,7 @@ const Body = () => {
 
             </div>
             <div className="card-container">
-                 {listOfMovies.map((movies,index) => (<MovieCard key={index} movieData = {movies}/> )
+                 {filteredMovies.map((movies,index) => (<MovieCard key={index} movieData = {movies}/> )
                 ) }
 
             </div>
